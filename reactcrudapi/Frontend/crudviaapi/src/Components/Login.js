0549ad9207f97b3f70911b604c0d7fd886f4b46a
Saif Ul islam <91761499+syousafzai861@users.noreply.github.com>
2023-01-19 13:27:01 +0500

    Login Logic Applied
    at this point to our application the login logic was applied now a user can login to our application and can view the content present into our application

--- reactcrudapi/Frontend/crudviaapi/src/Components/Login.js
+++ reactcrudapi/Frontend/crudviaapi/src/Components/Login.js
@@ -1,11 +1,42 @@
 import React, { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
-
+import axios from 'axios';
+import Swal from 'sweetalert2'
 function Login() {
     const [identifier, setIdentifier] = useState('');
     const [password, setPassword] = useState('');
-    let login = () =>{
+    let login = () => {
         console.log("i am loged in")
+        console.log(identifier);
+        console.log(password);
+        axios
+            .post('http://localhost:1337/api/auth/local', {
+                identifier: identifier,
+                password: password,
+            })
+            .then(response => {
+                // Handle success.
+
+                localStorage.setItem('token', response.jwt)
+                Swal.fire(
+                    'WelCome!',
+                    JSON.stringify(response.data.user.username),
+                    'You Are Logged In'
+                )
+                console.log('User profile', response.data.user);
+                console.log('User token', response.data.jwt);
+
+
+            })
+            .catch(error => {
+                // Handle error.
+                console.log('An error occurred:', error.response);
+                Swal.fire(
+                    'Oops!',
+                    JSON.stringify(error.response),
+                    'Sorry Provide Right Credentials...!!!!!'
+                )
+            });
     }
     return (
         <Container>
@@ -16,18 +47,18 @@ function Login() {
                     <Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" name="identifier" value={identifier} onChange={(e)=>{setIdentifier(e.target.value)}} placeholder="Enter email" />
-                           
+                            <Form.Control type="email" name="identifier" value={identifier} onChange={(e) => { setIdentifier(e.target.value) }} placeholder="Enter email" />
+
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" name="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder="Password" />
+                            <Form.Control type="password" name="password" value={password} onChange={(e) => { setPassword(e.target.value) }} placeholder="Password" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicCheckbox">
                             <Form.Check type="checkbox" label="Check me out" />
                         </Form.Group>
-                        <Button variant="primary" type="button" onClick={()=>{login()}}>
+                        <Button variant="primary" type="button" onClick={() => { login() }}>
                             LOGIN
                         </Button>
                     </Form>
